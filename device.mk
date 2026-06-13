8b03c0d5d38312972236b68c8cc9b6455d27e351
Mashopy <eliasgheeraert@gmail.com>
2026-06-13 18:53:52 +0700

    X6880: Inherit MSSI common overlays
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -300,14 +300,14 @@ PRODUCT_PACKAGES += \
     Tag
 
 # Overlays
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
     SettingsResTarget \
     SettingsProviderResTarget \
     SystemUIResTarget \
-    OpenDeltaOverlayMT6789 \
-    WifiResTarget
+    OpenDeltaOverlayMT6789
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -396,6 +396,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Radio
+ENABLE_VENDOR_RIL_SERVICE := true
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio@1.6.vendor
